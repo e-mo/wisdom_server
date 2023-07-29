63b69b891bc8b89fbd889cac00992b73367a4d73
emo <emorse8686@gmail.com>
2023-07-28 21:31:36 -0400

    Add echo to child

--- child.c
+++ child.c
@@ -7,6 +7,7 @@ int child_main(int client_fd, char *addr) {
 	printf("child: hello from %s\n", addr);
 
 	char *buf[100];
-	recv(client_fd, buf, 100, 0);	
-	printf("child: meow\n");
+	size_t num;
+	num = recv(client_fd, buf, 100, 0);	
+	printf("%.*s", num, buf);
 }
