91b06d2992ac96d893b7d267e3162952b4f10bb9
e-mo <emorse8686@gmail.com>
2024-03-28 20:44:44 -0400

    Use fcntl to set non blocking

--- child.c
+++ child.c
@@ -3,19 +3,31 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
 #include "child.h"
 
 int child_main(int client_fd, char *addr) {
 
 	printf("Child starting!\n");
+	fcntl(client_fd, F_SETFL, O_NONBLOCK);
 
 	char buf[100];
 	size_t num;
+
 	for (;;) {
 
-		num = recv(client_fd, buf, 100, MSG_DONTWAIT);	
-		if (num < 1) break;
+		num = recv(client_fd, buf, 100, 0);	
+		if (num < 1) {
+			if (errno != EAGAIN && errno != EWOULDBLOCK)
+				break;
 
+			printf("would have blocked\n");
+			usleep(1000 * 10);
+			continue;
+		}
 	}
 
 	printf("Child closing!\n");
